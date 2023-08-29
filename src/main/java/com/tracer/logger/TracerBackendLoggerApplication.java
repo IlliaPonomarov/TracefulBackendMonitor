f18309fb6ca0af537eabae45af6dfa63b21f02d2
Illia Ponomarov <chippaxyucac@gmail.com>
2023-08-29 18:21:46 +0200

    Swagger Configuration has been implemented.

--- src/main/java/com/tracer/logger/TracerBackendLoggerApplication.java
+++ src/main/java/com/tracer/logger/TracerBackendLoggerApplication.java
@@ -1,9 +1,28 @@
 package com.tracer.logger;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Trace Backend Logger API",
+                description = "Trace Backend Logger API Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Illia"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        )
+
+)
 public class TracerBackendLoggerApplication {
 
     public static void main(String[] args) {
