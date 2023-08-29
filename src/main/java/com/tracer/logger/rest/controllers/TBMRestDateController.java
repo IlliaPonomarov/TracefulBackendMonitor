4ebbeaf91a0c8208fe50c58bf078740da59797e7
Illia Ponomarov <chippaxyucac@gmail.com>
2023-08-29 16:12:17 +0200

    date controller has been modified.

--- src/main/java/com/tracer/logger/rest/controllers/TBMRestDateController.java
+++ src/main/java/com/tracer/logger/rest/controllers/TBMRestDateController.java
@@ -1,7 +1,8 @@
 package com.tracer.logger.rest.controllers;
 
-import com.tracer.logger.rest.dtos.TBLRestLogDTO;
+import com.tracer.logger.rest.dtos.TBMRestLogDTO;
 import com.tracer.logger.rest.exceptions.DateException;
+import com.tracer.logger.rest.exceptions.ServiceNotFoundException;
 import com.tracer.logger.rest.exceptions.TBMRestLogNotFounded;
 import com.tracer.logger.rest.mappers.TBMRestLogMapper;
 import com.tracer.logger.rest.models.TBMRestLog;
@@ -11,25 +12,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/rest/date")
 public class TBMRestDateController {
 
-    private final TBMRestService TBMRestService;
+    private final TBMRestService tbmRestService;
 
     @Autowired
-    public TBMRestDateController(TBMRestService TBMRestService) {
-        this.TBMRestService = TBMRestService;
+    public TBMRestDateController(TBMRestService tbmRestService) {
+        this.tbmRestService = tbmRestService;
     }
 
     @GetMapping("/{start}/{end}")
     @ResponseStatus(HttpStatus.FOUND)
-    public List<TBLRestLogDTO> findByDate(@PathVariable String start, @PathVariable String end) {
+    public List<TBMRestLogDTO> findRestLogByBetweenDateAndService(@PathVariable String start, @PathVariable String end, @RequestParam String service) {
 
         List<TBMRestLog> TBMRestLog = null;
+        Optional<TBMRestLog> tbmRestLog = tbmRestService.findByService(service);
+
+        if (tbmRestLog.isEmpty())
+            throw new ServiceNotFoundException(String.format("Service %s not found", service));
+
         try {
-            TBMRestLog = TBMRestService.findByDate(start, end);
+            TBMRestLog = tbmRestService.findRestLogByBetweenDateAndService(start, end, service);
         } catch (DateException e) {
             throw new DateException(e.getMessage());
         }
